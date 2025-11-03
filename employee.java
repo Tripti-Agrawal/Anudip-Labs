1e23f21c6f6464b161fc634f1c10f3c42a4929e1
Tripti-Agrawal <atripti379@gmail.com>
2025-11-03 10:24:17 +0530

    Add files via upload
    second

---
+++ employee.java
@@ -0,0 +1,11 @@
+public class employee {
+    protected int id;
+    protected int age;
+    protected String name;
+    protected boolean isPermanent;
+    public static void main(String[] args) {
+        employee empl = new employee();
+        empl.age = (int) 35.5;
+        System.out.println("Successfully started");
+    }
+}
